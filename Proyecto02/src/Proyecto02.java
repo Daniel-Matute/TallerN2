4207e2faf89534c903c021814f2c8dc3f2e45dfc
Daniel-Matute <danielmatetezs@gmail.com>
2024-10-20 12:58:49 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyecto02/src/Proyecto02.java
@@ -0,0 +1,7 @@
+public class Proyecto02 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java versionado a través de Git manejado desde GitHub además se usar GitHubClassroom");
+    }
+    
+}
